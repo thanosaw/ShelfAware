1e3efa48a0b801b0b2f8b1b88ba903ffceae6e5c
Andrew Wang <awang@adaptivecomputing.com>
2025-06-06 17:32:34 -0700

    levenshtein

--- merged_app_v2.py
+++ merged_app_v2.py
@@ -193,6 +193,28 @@ def normalize_food_name(name):
     name = ' '.join(name.split())
     return name
 
+def levenshtein(a, b):
+    """Calculate the Levenshtein distance between two strings.
+    This measures how many single-character edits are needed to change one string into another.
+    """
+    if len(a) < len(b):
+        return levenshtein(b, a)
+    if len(b) == 0:
+        return len(a)
+    
+    previous_row = range(len(b) + 1)
+    for i, c1 in enumerate(a):
+        current_row = [i + 1]
+        for j, c2 in enumerate(b):
+            insertions = previous_row[j + 1] + 1
+            deletions = current_row[j] + 1
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+    
+    return previous_row[-1]
+
+
 def name_sim(a, b):
     """Calculate similarity between two food names"""
     # Normalize both names
